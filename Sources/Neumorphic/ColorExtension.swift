f2dfda225b686cfd7a30a6d8c50b4f0a36130be7
boyb <macfifth@gmail.com>
2024-08-20 13:41:03 -0400

    Format file, update main colors to be 0ff-centric

--- Sources/Neumorphic/ColorExtension.swift
+++ Sources/Neumorphic/ColorExtension.swift
@@ -1,20 +1,27 @@
 import SwiftUI
 
 public extension Color {
-
     struct Neumorphic {
-        //Color
-        private static let defaultMainColor = NeumorphicKit.colorType(red: 0.925, green: 0.941, blue: 0.953)
-        private static let defaultSecondaryColor = NeumorphicKit.colorType(red: 0.482, green: 0.502, blue: 0.549)
-        private static let defaultLightShadowSolidColor = NeumorphicKit.colorType(red: 1.000, green: 1.000, blue: 1.000)
-        private static let defaultDarkShadowSolidColor = NeumorphicKit.colorType(red: 0.820, green: 0.851, blue: 0.902)
+        // Color
+        private static let defaultMainColor = NeumorphicKit
+            .colorType(red: 0, green: 0.0588, blue: 0.0588)
+        private static let defaultSecondaryColor = NeumorphicKit
+            .colorType(red: 0.475, green: 0.537, blue: 0.537)
+        private static let defaultLightShadowSolidColor = NeumorphicKit
+            .colorType(red: 1.000, green: 1.000, blue: 1.000)
+        private static let defaultDarkShadowSolidColor = NeumorphicKit
+            .colorType(red: 0.828, green: 0.898, blue: 0.898)
 
-        private static let darkThemeMainColor = NeumorphicKit.colorType(red: 0.188, green: 0.192, blue: 0.208)
-        private static let darkThemeSecondaryColor = NeumorphicKit.colorType(red: 0.910, green: 0.910, blue: 0.910)
-        private static let darkThemeLightShadowSolidColor = NeumorphicKit.colorType(red: 0.243, green: 0.247, blue: 0.275)
-        private static let darkThemeDarkShadowSolidColor = NeumorphicKit.colorType(red: 0.137, green: 0.137, blue: 0.137)
-                
-        public static var colorSchemeType : NeumorphicKit.ColorSchemeType {
+        private static let darkThemeMainColor = NeumorphicKit
+            .colorType(red: 0.188, green: 0.192, blue: 0.208)
+        private static let darkThemeSecondaryColor = NeumorphicKit
+            .colorType(red: 0.910, green: 0.910, blue: 0.910)
+        private static let darkThemeLightShadowSolidColor = NeumorphicKit
+            .colorType(red: 0.243, green: 0.247, blue: 0.275)
+        private static let darkThemeDarkShadowSolidColor = NeumorphicKit
+            .colorType(red: 0.137, green: 0.137, blue: 0.137)
+
+        public static var colorSchemeType: NeumorphicKit.ColorSchemeType {
             get {
                 return NeumorphicKit.colorSchemeType
             }
@@ -22,11 +29,11 @@ public extension Color {
                 NeumorphicKit.colorSchemeType = newValue
             }
         }
-        
+
         public static var main: Color {
             NeumorphicKit.color(light: defaultMainColor, dark: darkThemeMainColor)
         }
-        
+
         public static var secondary: Color {
             NeumorphicKit.color(light: defaultSecondaryColor, dark: darkThemeSecondaryColor)
         }
@@ -39,8 +46,4 @@ public extension Color {
             NeumorphicKit.color(light: defaultDarkShadowSolidColor, dark: darkThemeDarkShadowSolidColor)
         }
     }
-    
-
-    
-    
 }
